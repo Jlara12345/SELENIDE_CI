07a1a87fc5947cb1ed758e2206c3a26a422f4357
Juan Lara <jlara@quality-services.mx>
2025-07-03 11:44:35 -0600

    VERSION INICIAL

---
+++ src/test/java/tests/Test_Selenide_CI.java
@@ -0,0 +1,52 @@
+package tests;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.codeborne.selenide.Configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test_Selenide_CI {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	     WebDriverManager.chromedriver().setup();
+	        
+	        
+	        ChromeOptions options = new ChromeOptions();
+
+	        // Cabecera de un navegador real (moderna y válida)
+	        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+	                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36");
+	        
+
+	        DesiredCapabilities capabilities = new DesiredCapabilities();
+	        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+	        
+	        
+	        Configuration.browserCapabilities = capabilities;
+	        Configuration.browser = "chrome";
+	        
+	        Configuration.browser = "chrome";
+	        Configuration.headless = false; // true si estás en CI o sin entorno gráfico
+	        
+	        //Instruccion para maximizar el navegador
+	       // Configuration.startMaximized = true;
+	        
+	        open("https://www.google.com/");
+	        
+	        sleep(20000);
+	}
+
+}
